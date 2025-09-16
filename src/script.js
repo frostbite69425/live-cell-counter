d8f2fbc4773b8d4a11c40e2a9463bde5e64cb43d
frostbite69425 <medhihirak3@gmail.com>
2025-09-16 21:16:00 +0530

    feat: add individual counters for dead and live cells along with separate reset buttons

--- src/script.js
+++ src/script.js
@@ -4,22 +4,56 @@ let liveCellCount = document.body.querySelector(".live-cell-count");
 
 let deadCellCount = document.body.querySelector(".dead-cell-count");
 
-let incrementButton = document.body.querySelector(".increment-btn");
+let liveDecrementBtn = document.querySelector(".live-decrement-btn");
 
-let decrementButton = document.body.querySelector(".decrement-btn");
+let liveResetBtn = document.querySelector(".live-reset-btn");
 
-let resetButton = document.body.querySelector(".reset-btn");
+let liveIncrementBtn = document.querySelector(".live-increment-btn");
 
-incrementButton.addEventListener("click", () => {
+let deadDecrementBtn = document.querySelector(".dead-decrement-btn");
+
+let deadResetBtn = document.querySelector(".dead-reset-btn");
+
+let deadIncrementBtn = document.querySelector(".dead-increment-btn");
+
+let instaBtn = document.querySelector(".insta");
+
+// LISTENERS
+
+// LIVE CELL COUNTER
+
+liveIncrementBtn.addEventListener("click", () => {
   liveCount = Number(liveCellCount.textContent);
   liveCellCount.textContent = liveCount + 1;
 });
 
-decrementButton.addEventListener("click", () => {
+liveDecrementBtn.addEventListener("click", () => {
+  liveCount = Number(liveCellCount.textContent);
+  if (liveCount > 0) {
+    liveCellCount.textContent = liveCount - 1;
+  }
+});
+
+liveResetBtn.addEventListener("click", () => {
+  liveCellCount.textContent = 0;
+});
+
+deadIncrementBtn.addEventListener("click", () => {
   deadCount = Number(deadCellCount.textContent);
   deadCellCount.textContent = deadCount + 1;
 });
 
-resetButton.addEventListener("click", () => {
-  deadCellCount.textContent = liveCellCount.textContent = 0;
+deadDecrementBtn.addEventListener("click", () => {
+  deadCount = Number(deadCellCount.textContent);
+  if (deadCount > 0) {
+    deadCellCount.textContent = deadCount - 1;
+  }
+});
+
+deadResetBtn.addEventListener("click", () => {
+  deadCellCount.textContent = 0;
+});
+
+instaBtn.addEventListener("click", () => {
+  window.location.href = "https://www.instagram.com/frostbite.101/";
 });
